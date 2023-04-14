5cec60c7d7a40e884986486d89b1bec4a0174241
yeasintaha <meahmeah82@gmail.com>
2023-04-14 15:25:59 +0600

    crash fixing

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ def get_something():
 
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#    uvicorn.run(app, host='127.0.0.1', port=8000)
 
 #uvicorn main:app --reload
